ff0cea6208e672948c9c6ce030e646cb6cbe23aa
nataliaMykhailova <natalia.mykhailova.95@gmail.com>
2024-08-08 01:26:08 +0300

    commit-8

--- main.js
+++ main.js
@@ -42,6 +42,8 @@ document.querySelectorAll('.clear-icon').forEach(icon => {
         input.value = '';
         input.classList.remove('filled');
         input.focus();
+        const submitBtn = document.querySelector('.submit-btn');
+        submitBtn.style.display = 'block';
     });
 });
 
